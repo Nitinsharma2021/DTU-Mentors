7e6cb55b65d5959f2ce07683b99bcf6142ec22c3
hareshnayak <sketchharry01@gmail.com>
2022-01-01 16:56:16 +0530

    Fragments added

--- app/src/main/java/com/hareshnayak/dtumentors/ui/MainActivity.kt
+++ app/src/main/java/com/hareshnayak/dtumentors/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hareshnayak.dtumentors
+package com.hareshnayak.dtumentors.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hareshnayak.dtumentors.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
